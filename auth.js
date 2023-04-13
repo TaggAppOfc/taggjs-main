d26ce08e1a768bfc2c6861089aecaaa112b1163e
Gabriel Luís da Silva <79342117+mercuryVM@users.noreply.github.com>
2023-04-12 23:17:39 -0300

    fix

--- auth.js
+++ auth.js
@@ -3,12 +3,10 @@ const {Get} = require('./http');
 function LoginUser(username, password) {
     return new Promise((resolve, reject) => {
         Get("/auth/login").then((response) => {
-            const response = {
+            resolve({
                 statusCode: response.status,
                 token: response.data.token
-            }
-
-            resolve(response);
+            });
         }).catch((err) => {
             reject(err);
         })
